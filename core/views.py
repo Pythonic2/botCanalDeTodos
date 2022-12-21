a15903d10ca44e87968ba155f37e70e66e7bbb39
C1325058 Igor Marinho da Silva <c1325058@interno.bb.com.br>
2022-12-21 17:59:47 -0300

    make it better

--- core/views.py
+++ core/views.py
@@ -29,36 +29,36 @@ def home(request):
 
                 with sync_playwright() as p:
                     print('Iniciando os Acessos...')
-                    browser = p.chromium.launch(chromium_sandbox=False)
+                    browser = p.chromium.launch(headless=True,chromium_sandbox=False)
                     ua = UserAgent()
                     page = browser.new_page(user_agent=ua.random)
                     
                     page.goto("https://www.canaldopovo.com/")
-                    print(page.title)
-            #         lista = page.query_selector_all("//div[contains(text(),'Últimas notícias')]/../div/div/h3/a")
-            #         nova_lista = [i.get_attribute('href') for i in lista ]
-            #         titulos = list()
-            #         for i in nova_lista:
-            #             page = browser.new_page(user_agent=ua.random)
-            #             page.goto(i)
-            #             sleep(3)                        
-            #             page.evaluate('window.scrollTo(0,600)')
-            #             sleep(3)
-            #             page.evaluate('window.scrollTo(0,600)')
+                    
+                    lista = page.query_selector_all("//div[contains(text(),'Últimas notícias')]/../div/div/h3/a")
+                    nova_lista = [i.get_attribute('href') for i in lista ]
+                    titulos = list()
+                    for i in nova_lista:
+                        page = browser.new_page(user_agent=ua.random)
+                        page.goto(i)
+                        sleep(3)                        
+                        page.evaluate('window.scrollTo(0,600)')
+                        sleep(3)
+                        page.evaluate('window.scrollTo(0,600)')
 
-            #             titulos.append(page.title())
+                        titulos.append(page.title())
                       
-            #             # try:
-            #             #     banner = page.locator(f'//ins[contains(@data-revive-seq,"{randint(0,1)}")]/iframe')
-            #             #     page.goto(banner.get_attribute('src'))
-            #             #     page.evaluate('javascript:window.open(window.clickTag)')
-            #             # except:
-            #             #     pass
+                        # try:
+                        #     banner = page.locator(f'//ins[contains(@data-revive-seq,"{randint(0,1)}")]/iframe')
+                        #     page.goto(banner.get_attribute('src'))
+                        #     page.evaluate('javascript:window.open(window.clickTag)')
+                        # except:
+                        #     pass
 
-            #         print(len(titulos))
-            # resultado = Parallel(n_jobs=-1)(delayed(calcular)(arquivo) for arquivo in range(valor))
-            # print(resultado)
-            # print(f"Demorou:{time.time() - tempo_inicial}")
+                    print(len(titulos))
+            resultado = Parallel(n_jobs=-1)(delayed(calcular)(arquivo) for arquivo in range(valor))
+            print(resultado)
+            print(f"Demorou:{time.time() - tempo_inicial}")
 
     return render(request,'index.html')
 
