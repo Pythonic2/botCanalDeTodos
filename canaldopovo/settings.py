df502ec1f8be9b6cd28f195337c9b12b5a648fe8
C1325058 Igor Marinho da Silva <c1325058@interno.bb.com.br>
2022-12-20 22:00:02 -0300

    make it better

--- canaldopovo/settings.py
+++ canaldopovo/settings.py
@@ -74,7 +74,9 @@ WSGI_APPLICATION = 'canaldopovo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+default_dburl = 'sqlite:///' + BASE_DIR/ 'db.sqlite3'
 
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
