30e1a72be0f82c7c3db854a6f02f5d9a339aa8e5
C1325058 Igor Marinho da Silva <c1325058@interno.bb.com.br>
2022-12-20 22:27:46 -0300

    make it better

--- canaldopovo/settings.py
+++ canaldopovo/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =config('SECRET_KEY')
+SECRET_KEY ='django-insecure-7#jd4)ok^()^+ajmw379*dpjmny!b+7y4*7f%7!c^hqdc$8c+z'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['bot-canal-povo.herokuapp.com/']
 
