08e5771773d5b019ed9904fa9c086a05e590b87a
C1325058 Igor Marinho da Silva <c1325058@interno.bb.com.br>
2022-12-22 15:55:00 -0300

    mmm

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('',views.CanalDeTodos.as_view(),name='canaldetodos'),
     
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.views.generic import ListView
 from datetime import datetime
 # Create your views here.
 from time import sleep
@@ -16,46 +17,13 @@ tempo_inicial = time.time()
 subprocess.run(["playwright", "install", "chromium"])
 
 def home(request): 
-    v = request.POST.get("nome")
-    if v == None or v == '':
-        pass
-    else:
-        valor = int(v)
-        if request.method == 'POST':
-    
-            def calcular(arquivo):
-                with sync_playwright() as p:
-                    
-                    browser = p.chromium.launch(headless=True,chromium_sandbox=False)
-                    ua = UserAgent()
-                    page = browser.new_page(user_agent=ua.random)
-                    
-                    page.goto("https://www.canaldopovo.com/")
-                    
-                    lista = page.query_selector_all("//div[contains(text(),'Últimas notícias')]/../div/div/h3/a")
-                    nova_lista = [i.get_attribute('href') for i in lista ]
-                    
-                    for i in nova_lista:    
-                        print('Abrindo mais uma aba')
-                        page.evaluate(f"window.open('{i}', '_blank')")
-                        sleep(4)                        
-                        
-
-                        
-                      
-                        # try:
-                        #     banner = page.locator(f'//ins[contains(@data-revive-seq,"{randint(0,1)}")]/iframe')
-                        #     page.goto(banner.get_attribute('src'))
-                        #     page.evaluate('javascript:window.open(window.clickTag)')
-                        # except:
-                        #     pass
-
-                  
-            resultado = Parallel(n_jobs=2)(delayed(calcular)(arquivo) for arquivo in range(valor))
-            print(f"Demorou:{time.time() - tempo_inicial}")
+   
 
     return render(request,'index.html')
 
+
+class CanalDeTodos(ListView):
+    
 
 
 
