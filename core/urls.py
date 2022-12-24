565807d36c95820eb19adaf195d5566d530fb8db
C1325058 Igor Marinho da Silva <c1325058@interno.bb.com.br>
2022-12-24 18:56:20 -0300

    make it better

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.CanalDeTodos.as_view(),name='canaldetodos'),
+    path('',views.home,name='canaldetodos'),
     
 ]
